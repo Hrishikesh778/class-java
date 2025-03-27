1e6438062ce9fe382a35496bf2be099da8ad27a2
Hrishikesh778 <hrishi778@gmail.com>
2025-03-27 23:25:35 +0530

    first version

---
+++ java classes/Conversion.java
@@ -0,0 +1,24 @@
+package com.myfirstproj;
+
+public class Conversion {
+
+	public static void main(String[] args) {
+		
+		
+		int a=25;
+		System.out.println(a);
+		
+		float f=a;
+		System.out.println(f);
+		
+		float ft =99.75f;
+		System.out.println(ft);
+		
+		int i =(int) ft;
+		System.out.println(i);
+		
+		float div =(float) 5/25;
+		System.out.println(div);
+	}
+	
+}
---
+++ java classes/patternpractice.java
@@ -0,0 +1,67 @@
+package com.myfirstproj;
+
+public class patternpractice {
+
+	public static void main(String[] args) {
+
+		int rd = 5;
+
+		for (int r = 1; r <= rd; r++) {
+
+			for (int c = 1; c <= r; c++) {
+				System.out.print("*");
+
+			}
+			System.out.println("");
+
+		}
+
+		System.out.println(">>>>>>>>>>>>>>>>>>>>");
+		
+		for (int r = 1; r <= rd; r++) {
+
+			for (int c = 1; c <= rd-r+1; c++) {
+				System.out.print("*");
+
+			}
+			System.out.println("");
+
+		}
+		
+		System.out.println(">>>>>>>>>>>>>>>>>>>>");
+		
+		for (int r = 1; r <= rd; r++) {
+
+			for (int c = 1; c <= rd-r; c++) {
+				System.out.print(" ");
+
+			}
+			for (int c = 1; c <= r; c++) {
+				System.out.print("*");
+
+			}
+			System.out.println("");
+
+		}
+		
+		System.out.println(">>>>>>>>>>>>>>>>>>>>");
+		
+
+		for (int r = 1; r <= rd; r++) {
+
+			for (int c=1;c<r;c++) {
+		    	System.out.print(" ");
+		    }
+			
+		    for (int c=1;c<=rd-r+1;c++) {
+		    	System.out.print("*");
+		    }
+		    
+		    
+			System.out.println("");
+
+		}
+		
+	}
+
+}
